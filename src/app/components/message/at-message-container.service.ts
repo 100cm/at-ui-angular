4a7075e51039fb5af6bed0d76223d7e86d7422ac
icepoint1999 <351711778@qq.com>
2018-12-27 10:47:19 +0800

    fix message error

--- src/app/components/message/at-message-container.service.ts
+++ src/app/components/message/at-message-container.service.ts
@@ -1,20 +1,22 @@
-import {Injectable} from '@angular/core';
-import {ComponentCreator} from "../core/component-creator";
-import {MessageContainerComponent} from "./message-container/message-container.component";
-import {ComponentCreatorBase} from "../core/component-creator-base";
+import {Injectable}                from '@angular/core';
+import {ComponentCreator}          from '../core/component-creator';
+import {MessageContainerComponent} from './message-container/message-container.component';
+import {ComponentCreatorBase}      from '../core/component-creator-base';
 
 @Injectable({
-              providedIn:'root'
-            })
+  providedIn: 'root'
+})
 export class AtMessageContainerService extends ComponentCreator<MessageContainerComponent> {
+
+  messageComponentRef
 
   constructor(public component_base: ComponentCreatorBase) {
     super(component_base, MessageContainerComponent)
-    this.create()
+    this.messageComponentRef = this.create()
   }
 
   addMessage(config) {
-    this.getElem().instance.addMessage(config)
+    this.messageComponentRef.instance.addMessage(config)
   }
 
 }
