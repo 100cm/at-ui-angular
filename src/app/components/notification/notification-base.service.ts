f4ed920a4929d44b0b794bd22cab3608a4956548
icepoint1999 <351711778@qq.com>
2018-12-27 10:52:17 +0800

    fix notification

--- src/app/components/notification/notification-base.service.ts
+++ src/app/components/notification/notification-base.service.ts
@@ -2,14 +2,14 @@ import {ComponentRef, Injectable}       from '@angular/core';
 import {ComponentCreator}               from '../core/component-creator';
 import {NotificationContainerComponent} from './notification-container/notification-container.component';
 import {ComponentCreatorBase}           from '../core/component-creator-base';
+import {NotificationComponent}          from './notification/notification.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationBaseService extends ComponentCreator<NotificationContainerComponent> {
 
-  notificationComponentRef: ComponentRef
-
+  notificationComponentRef
   constructor(public component_base: ComponentCreatorBase) {
     super(component_base, NotificationContainerComponent)
     this.notificationComponentRef = this.create()
