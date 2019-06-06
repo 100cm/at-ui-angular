1841c008ebed7503259e2cb645ccffba35fe2dd9
icepoint1999 <351711778@qq.com>
2019-06-06 12:22:37 +0800

    upgrade new theme

--- src/app/showcase/at-demo-theme/at-demo-theme.component.ts
+++ src/app/showcase/at-demo-theme/at-demo-theme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { getHue, getSaturation, getValue } from '../../utils/color';
+import tinycolor from 'tinycolor2';
 
 @Component({
   selector: 'app-at-demo-theme',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtDemoThemeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    const pColor = tinycolor('#1890ff');
+    console.log(pColor);
+    console.log(pColor.toHsv())
+    console.log(getHue(pColor.toHsv(), 1, false));
+    console.log(getSaturation(pColor.toHsv(), 1, false));
+    console.log(getValue(pColor.toHsv(), 1, false));
+    const c = tinycolor({
+      h: getHue(pColor.toHsv(), 1, false),
+      s: getSaturation(pColor.toHsv(), 1, false),
+      v: getValue(pColor.toHsv(), 1, false)
+    });
+    console.log(c.toHsl());
+    console.log(c.toHexString());
   }
 
 }
