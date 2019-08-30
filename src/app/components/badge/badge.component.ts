6cc692832b6961cf9b39c87dc958f17ca5d87c0b
loststars20160827 <490483921@qq.com>
2019-08-30 16:03:22 +0800

    badge test passed

--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -3,20 +3,16 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 @Component({
   selector: 'at-badge',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<span class="at-badge at-badge--{{atType}}">
-  <span #content>
-  <ng-content>
-
-  </ng-content>
-  </span>
-  <span *ngIf="!dot && show" class="at-badge"
-        [ngClass]="{'at-badge--corner':(content.innerText.length > 0 || content.children.length >0),
-        'at-badge--alone':(content.innerText.length == 0 && content.children.length == 0 )}">
-  <sup class="at-badge__content" [ngClass]="{'at-badge--dot':dot}">{{dot ? '' : atValue}}</sup>
-  </span>
-    <sup *ngIf="dot && show" class="at-badge__content" [ngClass]="{'at-badge--dot':dot,'at-badge--corner':(content.innerText.length > 0 || content.children.length >0),
-        'at-badge--alone':(content.innerText.length == 0 && content.children.length == 0 )}">{{dot ? '' : atValue}}</sup>
-</span>
+  template: `
+    <span class="at-badge at-badge--{{atType}}">
+      <span #content>
+        <ng-content></ng-content>
+      </span>
+      <span class="at-badge" [ngClass]="{'at-badge--corner':(content.innerText.length > 0 || content.children.length >0),
+        'at-badge--alone':(content.innerText.length == 0 && content.children.length == 0 )}" >
+        <sup *ngIf="show" class="at-badge__content" [ngClass]="{'at-badge--dot':dot}">{{dot ? '' : atValue}}</sup>
+      </span>
+    </span>
   `
 })
 export class BadgeComponent implements OnInit {
@@ -44,6 +40,7 @@ export class BadgeComponent implements OnInit {
   @Input()
   set atValue(value: number | string) {
     this._atValue = value;
+    this.show = typeof value === 'number' ? value >= 0 : true;
   }
 
 }
